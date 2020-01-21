cf2d92ddb85b7a29f2bda1c45d77264f53d2ba8c
jheisson.santos <jh.eison@hotmail.com>
2020-01-20 21:57:56 -0300

    Iniciando projeto

---
+++ src/test/java/com/jhei/cursomc/cursoMc/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhei.cursomc.cursoMc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
