e1dddf42ede339877f7d5cf6aee712839007c898
jheisson.santos <jh.eison@hotmail.com>
2020-01-21 22:47:12 -0300

    Primeiro serviço

--- src/main/java/com/jhei/cursomc/cursoMc/CursoMcApplication.java
+++ src/main/java/com/jhei/cursomc/cursoMc/CursoMcApplication.java
@@ -1,13 +1,13 @@
-package com.jhei.cursomc.cursoMc;
+package com.jhei.cursomc.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoMcApplication {
+public class CursomcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoMcApplication.class, args);
+		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 }
