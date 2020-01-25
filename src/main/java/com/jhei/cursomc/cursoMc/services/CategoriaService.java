38b8af17a4dca6167f504f0e6ade6ac78ceb1d2f
jheisson.santos <jh.eison@hotmail.com>
2020-01-25 11:20:19 -0300

    Adicionando exception handler para mapeamento e tratamento de excessões

--- src/main/java/com/jhei/cursomc/cursoMc/services/CategoriaService.java
+++ src/main/java/com/jhei/cursomc/cursoMc/services/CategoriaService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import com.jhei.cursomc.cursomc.domain.Categoria;
 import com.jhei.cursomc.cursomc.repositories.CategoriaRepository;
+import com.jhei.cursomc.cursomc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -17,7 +19,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return  obj.orElse(null);
+		return  obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id:" + id + ", Tipo" + Categoria.class.getName()));
 	}
 	
 	public List<Categoria> buscar() {
