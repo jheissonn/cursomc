e5721f97b0f0eb379ee2f2e3b4c50e445ed1ff60
jheisson.santos <jh.eison@hotmail.com>
2020-01-27 21:52:33 -0300

    Adicionando o primeiro DTO para categoria

--- src/main/java/com/jhei/cursomc/cursoMc/resources/CategoriaResource.java
+++ src/main/java/com/jhei/cursomc/cursoMc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.jhei.cursomc.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jhei.cursomc.cursomc.domain.Categoria;
+import com.jhei.cursomc.cursomc.dto.CategoriaDTO;
 import com.jhei.cursomc.cursomc.services.CategoriaService;
 
 @RestController
@@ -25,8 +27,10 @@ public class CategoriaResource {
 	CategoriaService categoriaService;
 	
 	@GetMapping
-	public ResponseEntity<List<Categoria>> listar() {		
-		return ResponseEntity.ok().body(categoriaService.find());
+	public ResponseEntity<List<CategoriaDTO>> findAll() {		
+		List<Categoria> list = categoriaService.find();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
