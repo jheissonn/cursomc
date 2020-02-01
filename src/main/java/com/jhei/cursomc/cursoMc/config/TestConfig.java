cd7d9d8fc8e2301b795c4ab7be988c8b3708ff03
jheisson.santos <jh.eison@hotmail.com>
2020-02-01 19:10:25 -0300

    Criando profile de teste

---
+++ src/main/java/com/jhei/cursomc/cursoMc/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.jhei.cursomc.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jhei.cursomc.cursomc.services.DbService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DbService service;
+
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		
+		service.instantiateTestDataBase();
+		
+		return true;
+		
+	}
+}
