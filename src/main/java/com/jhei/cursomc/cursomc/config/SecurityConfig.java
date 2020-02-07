cc3cd7512e32f278cee49f19c3eaad60f30420d2
jheisson.santos <jh.eison@hotmail.com>
2020-02-06 23:58:40 -0200

    Implementando autorização.

--- src/main/java/com/jhei/cursomc/cursomc/config/SecurityConfig.java
+++ src/main/java/com/jhei/cursomc/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.jhei.cursomc.cursomc.security.JWTAuthenticationFilter;
+import com.jhei.cursomc.cursomc.security.JWTAuthorizationFilter;
 import com.jhei.cursomc.cursomc.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHER).permitAll()	
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
